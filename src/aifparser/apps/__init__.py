5d4ed20e76ba18b3a020fbce6ef9e242e2fb4063
Ron Dockhorn <16195016+Bondoki@users.noreply.github.com>
2026-03-09 11:55:49 +0100

    ruff the apps

--- src/aifparser/apps/__init__.py
+++ src/aifparser/apps/__init__.py
@@ -1,28 +1,4 @@
 from nomad.config.models.plugins import AppEntryPoint
-from nomad.config.models.ui import App, Column, Columns, FilterMenu, FilterMenus
-
-# app_entry_point = AppEntryPoint(
-#     name='NewApp',
-#     description='New app entry point configuration.',
-#     app=App(
-#         label='NewApp',
-#         path='app',
-#         category='simulation',
-#         columns=Columns(
-#             selected=['entry_id'],
-#             options={
-#                 'entry_id': Column(),
-#             },
-#         ),
-#         filter_menus=FilterMenus(
-#             options={
-#                 'material': FilterMenu(label='Material'),
-#             }
-#         ),
-#     ),
-# )
-
-from nomad.config.models.plugins import AppEntryPoint
 from nomad.config.models.ui import (
     App,
     Axis,
@@ -31,13 +7,14 @@ from nomad.config.models.ui import (
     Layout,
     Markers,
     Menu,
-    MenuItemPeriodicTable,
     MenuItemHistogram,
+    MenuItemPeriodicTable,
     MenuItemTerms,
     SearchQuantities,
     WidgetHistogram,
     WidgetScatterPlot,
 )
+
 schema = 'aifparser.schema_packages.aif_schema_package.AdsorptionInformationFile'
 # schema = 'nomad_example.schema_packages.mypackage.MySchema'
 aif_app_entry_point = AppEntryPoint(
--- tests/apps/test_app.py
+++ tests/apps/test_app.py
@@ -2,4 +2,4 @@ def test_importing_app():
     # this will raise an exception if pydantic model validation fails for the app
     from aifparser.apps import aif_app_entry_point
 
-    assert aif_app_entry_point.app.label == 'AIF'
+    assert aif_app_entry_point.app.label == 'Adsorption Information File (AIF)'
