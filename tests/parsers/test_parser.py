79cdc6260f0e24519f77e0b8fd7191d7906702cc
Ron Dockhorn <16195016+Bondoki@users.noreply.github.com>
2026-03-09 14:41:35 +0100

    ruff test_parser.py

--- tests/parsers/test_parser.py
+++ tests/parsers/test_parser.py
@@ -1,11 +1,6 @@
-import logging
 import os.path
-import shutil
 
-from nomad.datamodel import EntryArchive
 from nomad.client import normalize_all, parse
-
-from aifparser.parsers.aif_parser import AIFParser
 
 
 def test_parse_file():
@@ -20,7 +15,7 @@ def test_parse_file():
     test_file = os.path.join('tests', 'data', 'anie202513606-sup-0002-suppmat2/ZIF8_GCMC_UFFTRAPPE.aif')
     entry_archive = parse(test_file)[0]
     normalize_all(entry_archive)
-    print(entry_archive.m_to_dict())
+    # print(entry_archive.m_to_dict())
     # Get the 'main section' section_run as a metainfo object
     #assert entry_archive.data.aif_operator == 'test_file_SEM_01.tif' #'testSEM.tif' 
     # assert archive.workflow2.name == 'test'
